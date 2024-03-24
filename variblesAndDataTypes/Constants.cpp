8c1c2228664c481b2fe65e8ae9563fbbb9558eaa
Omikhe <msakamike84@gmail.com>
2024-03-25 00:38:20 +0200

    Implement Constants

--- variblesAndDataTypes/Constants.cpp
+++ variblesAndDataTypes/Constants.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-//your code here
-return 0;
+    const int LENGTH = 10;
+    const int WIDTH = 5;
+    const char NEWLINE = '\n';
+    int area;
+    
+    area = LENGTH * WIDTH;
+    cout << area;
+    cout << NEWLINE;
+
+    return 0;
 }
