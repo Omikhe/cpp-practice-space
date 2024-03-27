e40a76a5e93c1526985ed85ea7c633c63599eda8
Omikhe <msakamike84@gmail.com>
2024-03-27 12:49:04 +0200

    Implement recursion: sum of n natural numbers

--- recursion/recursion_0.cpp
+++ recursion/recursion_0.cpp
@@ -2,11 +2,25 @@
 using namespace std;
 
 int nSum(int n){
+    // base condition to terminate the recursion when N = 0
+    if (n == 0) {
+        return 0;
+    }
 
+    // recursive case / recursive call
+    int res = n + nSum(n - 1);
+
+    return res;
 }
 
 int main()
 {
-//your code here
-return 0;
+    int n = 4;
+
+    // calling the function
+    int sum = nSum(n);
+
+    cout << "Sum = " << sum;
+
+    return 0;
 }
